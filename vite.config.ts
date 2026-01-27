b9bc5f1c3c82bf3c9bd2522a5ddba4d0dc12d7ad
odyssey <odissey.meltonyan@gmail.com>
2026-01-27 23:03:54 +0300

    update base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'odik8.github.io',
+  base: '/odik8.github.io/',
   build: {
     outDir: 'docs'
   }
